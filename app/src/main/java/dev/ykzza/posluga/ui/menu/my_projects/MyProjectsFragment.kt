4d69d4f4efd03fbab8cf2d71673b935de5fcc40a
ykzZa <n.hryshaienko@gmail.com>
2024-06-01 15:55:33 +0300

    dagger annotations

--- app/src/main/java/dev/ykzza/posluga/ui/menu/my_projects/MyProjectsFragment.kt
+++ app/src/main/java/dev/ykzza/posluga/ui/menu/my_projects/MyProjectsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
 import dev.ykzza.posluga.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,6 +18,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MyProjectsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+@AndroidEntryPoint
 class MyProjectsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
